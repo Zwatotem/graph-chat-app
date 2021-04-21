0a0f1fd8cea7c71b60e40c044505d4cb8a5cbcd9
Piotr Czamara <pczamara@gmail.com>
2021-04-21 12:06:34 +0200

    Add tests for class Conversation

--- chatAppTest/chatAppTest.cs
+++ chatAppTest/chatAppTest.cs
@@ -96,11 +96,12 @@ namespace chatAppTest
 			User user2 = chatSystem.addUser("Kasia èdüb≥o");
 			Conversation savedConversation = chatSystem.addConversation("Konfa 1", user1, user2);
 			Content msgContent1 = new TextContent("Heeejoooo");
-			Message sentMessage1 = chatSystem.sendMessage(savedConversation.getId(), "Jaú Kowalski", -1, msgContent1);
+			DateTime datetime = new DateTime(now);
+			Message sentMessage1 = chatSystem.sendMessage(savedConversation.getId(), "Jaú Kowalski", -1, msgContent1, datetime);
 			Assert.IsNotNull(sentMessage1);
 			Assert.IsNull(sentMessage1.getParent());
 			Content msgContent2 = new TextContent("CzeúÊ");
-			Message sentMessage2 = chatSystem.sendMessage(savedConversation.getId(), "Kasia èdüb≥o", sentMessage1.getId(), msgContent2);
+			Message sentMessage2 = chatSystem.sendMessage(savedConversation.getId(), "Kasia èdüb≥o", sentMessage1.getId(), msgContent2, datetime);
 			Assert.IsNotNull(sentMessage2);
 			Assert.IsTrue(sentMessage2.getParent() == sentMessage1); //test comment
 		}
@@ -109,7 +110,7 @@ namespace chatAppTest
 	[TestClass]
 	public class ServerChatSystemTest
 	{
-		public void getUpdatesToUserTest()
+		public void getUpdatesToUserTest() //do przejrzenia czy na pewno tak to robimy
         {
 			ServerChatSystem chatSystem = new ServerChatSystem();
 			User user1 = chatSystem.addUser("Jaú Kowalski");
@@ -119,14 +120,15 @@ namespace chatAppTest
 			Conversation savedConversation2 = chatSystem.addConversation("Konfa 2", user1, user3);
 			Content msgContent1 = new TextContent("Heeejoooo");
 			Content msgContent2 = new TextContent("Heeej");
-			Message sentMessage1 = chatSystem.sendMessage(savedConversation.getId(), "Jaú Kowalski", -1, msgContent1);
-			Message sentMessage2 = chatSystem.sendMessage(savedConversation2.getId(), "Jaú Kowalski", -1, msgContent2);
+			DateTime datetime = new DateTime(now);
+			Message sentMessage1 = chatSystem.sendMessage(savedConversation.getId(), "Jaú Kowalski", -1, msgContent1, datetime);
+			Message sentMessage2 = chatSystem.sendMessage(savedConversation2.getId(), "Jaú Kowalski", -1, msgContent2, datetime);
 			ICollection updates = chatSystem.getUpdatesOfUser("Kasia èdüb≥o"); //czy na pewno tak przechowujemy updaty?
 			bool containsConversation = false; 
 			bool containsWrongConversation = false;
-			foreach (var update in updates.getConversationUpdates()) //czy aby tak?
+			foreach (var update in updates.getConversations()) //czy aby tak?
             {
-				if (update.getId() == savedConversation.getId())
+				if (update.getId() == savedConversation.getId() && update.getName() == savedConversation.getName())
                 {
 					containsConversation = true;
 					bool containsMessage = false;
@@ -136,6 +138,10 @@ namespace chatAppTest
 						if (message.getId() == sentMessage1.getId())
                         {
 							containsMessage = true;
+							Assert.isTrue(message.getUser().getName() == sentMessage1.getUser().getName());
+							Assert.isNull(message.getParent());
+							Assert.isTrue(message.getContent().getData() == sentMessage1.getContent().getData());
+							Assert.isTrue(message.getTime() == sentMessage1.getTime());
                         }
 						else
                         {
@@ -239,12 +245,13 @@ namespace chatAppTest
 			User user2 = chatSystem.addUser("Kasia èdüb≥o");
 			Conversation savedConversation = chatSystem.addConversation("Konfa 1", user1, user2);
 			Content msgContent1 = new TextContent("Heeejoooo");
-			Message sentMessage1 = chatSystem.sendMessage(savedConversation.getId(), "Jaú Kowalski", -1, msgContent1);
+			DateTime datetime = new DateTime(now);
+			Message sentMessage1 = chatSystem.sendMessage(savedConversation.getId(), "Jaú Kowalski", -1, msgContent1, datetime);
 
 			ClientChatSystemTest clientChatSystem = new ClientChatSystemTest();
 			clientChatSystem.applyUpdates(chatSystem.getUpdatesOfUser("Kasia èdüb≥o"));
 			bool conversationPresent = false;
-			foreach (var conversation in clientChatSystem.getConversationsOfUser("Kasiaèdüb≥o"))
+			foreach (var conversation in clientChatSystem.getConversationsOfUser("Kasia èdüb≥o"))
             {
 				if (conversation.getId() == savedConversation.getId())
                 {
@@ -364,21 +371,6 @@ namespace chatAppTest
             }
 			Assert.isFalse(hasWrongConversation);
 			hasWrongConversation = false;
-			bool hasUser1 = false;
-			bool hasUser2 = false;
-			bool hasWrongUser = false;
-			foreach (var user in conversation1.getUsers())
-            {
-				hasWrongUser = true;
-            }
-			Assert.isFalse(hasWrongUser);
-			hasWrongUser = false;
-			foreach (var user in conversation2.getUsers())
-            {
-				hasWrongUser = true;
-            }
-			Assert.isFalse(hasWrongUser);
-			hasWrongUser = false;
 
 			bool methodResult;
 			methodResult = user1.matchWithConversation(conversation1);
@@ -413,9 +405,139 @@ namespace chatAppTest
 			Assert.isTrue(hasConversation2);
 			Assert.isFalse(hasWrongConversation);
 
-			hasUser1 = false;
-			hasUser2 = false;
+			methodResult = user2.matchWithConversation(conversation1);
+			Assert.isFalse(methodResult);
+        }
+
+		public void unmatchWithConversationTest()
+        {
+			Conversation conversation1 = new Conversation("Konfa 1", 1);
+			User user1 = new User("Pan A");
+
+			bool hasConversation1 = false;
+			bool hasWrongConversation = false;
+			foreach (var conversation in user1.getConversations())
+            {
+				hasWrongConversation = true;
+            }
+			Assert.isFalse(hasWrongConversation);
+			hasWrongConversation = false;
+
+			user1.matchWithConversation(conversation1);
+
+			foreach (var conversation in user1.getConversations())
+            {
+				if (conversation == conversation1) 
+					hasConversation1 = true;
+				else
+					hasWrongConversation = true;
+            }
+			Assert.isTrue(hasConversation1);
+			Assert.isFalse(hasWrongConversation);
+			hasConversation1 = false;
+			hasWrongConversation = false;
+
+			bool methodResult;
+			methodResult = user1.unmatchWithConversation(conversation1);
+			Assert.isTrue(methodResult);			
+
+			foreach (var conversation in user1.getConversations())
+            {
+				hasWrongConversation = true;
+            }
+			Assert.isFalse(hasWrongConversation);
+			hasWrongConversation = false;
+
+			methodResult = user1.unmatchWithConversation(conversation1);
+			Assert.isFalse(methodResult);	
+        }
+	}
+
+	[TestClass]
+	public class ConversationTest
+	{
+		[TestMethod] //czy taka anotacja powinna byÊ przy wszystkich?
+		public void getNameTest() 
+		{
+			string name = "Konwersacja 1";
+			ConversationTest conversation1 = new Conversation(name, 1);
+			Assert.isTrue(name == conversation1.getName());
+		}
+
+		public void addMessageTest()
+        {
+			User user1 = new User("Roch Lancel");
+			User user2 = new User("Harry Potter");
+			Conversation conversation1 = new Conversation("Konfa 1", 1);
+			Content msgContent1 = new TextContent("Heeejoooo");
+			DateTime datetime = new DateTime(now);
+			Message addedMessage = conversation1.addMessage(user1, -1, msgContent1, datetime, 1);
+			Assert.isNull(addedMessage);
+			conversation1.matchWithUser(user1);
+			Message addedMessage = conversation1.addMessage(user1, -1, msgContent1, datetime, 1);
+			Assert.isNotNull(addedMessage);
+			Assert.isTrue(user1 == addedMessage.getUser());
+			Assert.isNull(addedMessage.getParent());
+			Assert.isTrue(msgContent1 == addedMessage.getContent());
+			Assert.isTrue(datetime == addedMessage.getTime());
+			Assert.isTrue(1 == addedMessage.getId());
+
+			conversation1.matchWithUser(user2);
+			Content msgContent2 = new TextContent("Heeejoooo");
+			DateTime datetime2 = new DateTime(now);
+			Message nextMessage = conversation1.addMessage(user2, 1, msgContent2, datetime2, 2);
+			Assert.isNotNull(nextMessage);
+			Assert.isTrue(user2 == nextMessage.getUser());
+			Assert.isTrue(addedMessage == nextMessage.getParent());
+			Assert.isTrue(msgContent2 == nextMessage.getContent());
+			Assert.isTrue(datetime2 == nextMessage.getTime());
+			Assert.isTrue(2 == nextMessage.getId());
+        }
+
+		public void getMessageTest()
+        {
+			User user1 = new User("Roch Lancel");
+			Conversation conversation1 = new Conversation("Konfa 1", 1);
+			conversation1.matchWithUser(user1);
+			Assert.isNull(conversation1.getMessage(1));
+			Content msgContent1 = new TextContent("Heeejoooo");
+			DateTime datetime = new DateTime(now);
+			Message addedMessage = conversation1.addMessage(user1, -1, msgContent1, datetime, 1);
+			Assert.isTrue(addedMessage == conversation1.getMessage(1));
+			Assert.isNull(conversation1.getMessage(2));
+        }
+
+		public void matchWithUserTest()
+        {
+			Conversation conversation1 = new Conversation("Konfa 1", 1);
+			Conversation conversation2 = new Conversation("Konfa 2", 2);
+			User user1 = new User("Pan A");
+			user user2 = new user("Pani B");
+			
+			bool hasUser1 = false;
+			bool hasUser2 = false;
+			bool hasWrongUser = false;
+			foreach (var user in conversation1.getUsers())
+            {
+				hasWrongUser = true;
+            }
+			Assert.isFalse(hasWrongUser);
 			hasWrongUser = false;
+			foreach (var user in conversation2.getUsers())
+            {
+				hasWrongUser = true;
+            }
+			Assert.isFalse(hasWrongUser);
+			hasWrongUser = false;
+
+			bool methodResult;
+			methodResult = conversation1.matchWithUser(user1);
+			Assert.isTrue(methodResult);
+			methodResult = conversation1.matchWithUser(user2);
+			Assert.isTrue(methodResult);
+			methodResult = conversation2.matchWithUser(user1);
+			Assert.isTrue(methodResult);		
+
 			foreach (var user in conversation1.getUsers())
             {
 				if (user == user1)
@@ -441,23 +563,15 @@ namespace chatAppTest
 			Assert.isTrue(hasUser1);
 			Assert.isFalse(hasWrongUser);
 
-			methodResult = user2.matchWithConversation(conversation1);
+			methodResult = conversation2.matchWithUser(user1);
 			Assert.isFalse(methodResult);
         }
 
-		public void unmatchWithConversationTest()
+		public void unmatchWithUserTest()
         {
 			Conversation conversation1 = new Conversation("Konfa 1", 1);
 			User user1 = new User("Pan A");
 
-			bool hasConversation1 = false;
-			bool hasWrongConversation = false;
-			foreach (var conversation in user1.getConversations())
-            {
-				hasWrongConversation = true;
-            }
-			Assert.isFalse(hasWrongConversation);
-			hasWrongConversation = false;
 			bool hasUser1 = false;
 			bool hasWrongUser = false;
 			foreach (var user in conversation1.getUsers())
@@ -467,22 +581,8 @@ namespace chatAppTest
 			Assert.isFalse(hasWrongUser);
 			hasWrongUser = false;
 
-			user1.matchWithConversation(conversation1);
+			conversation1.matchWithUser(user1);
 
-			foreach (var conversation in user1.getConversations())
-            {
-				if (conversation == conversation1) 
-					hasConversation1 = true;
-				else
-					hasWrongConversation = true;
-            }
-			Assert.isTrue(hasConversation1);
-			Assert.isFalse(hasWrongConversation);
-			hasConversation1 = false;
-			hasWrongConversation = false;
-
-			hasUser1 = false;
-			hasWrongUser = false;
 			foreach (var user in conversation1.getUsers())
             {
 				if (user == user1)
@@ -496,34 +596,179 @@ namespace chatAppTest
 			hasWrongUser = false;
 
 			bool methodResult;
-			methodResult = user1.unmatchWithConversation(conversation1);
+			methodResult = conversation1.unmatchWithUser(user1);
 			Assert.isTrue(methodResult);			
 
-			foreach (var conversation in user1.getConversations())
-            {
-				hasWrongConversation = true;
-            }
-			Assert.isFalse(hasWrongConversation);
-			hasWrongConversation = false;
 			foreach (var user in conversation1.getUsers())
             {
 				hasWrongUser = true;
             }
 			Assert.isFalse(hasWrongUser);
 			hasWrongUser = false;
-			methodResult = user1.unmatchWithConversation(conversation1);
+
+			methodResult = conversation1.unmatchWithUser(user1);
 			Assert.isFalse(methodResult);	
         }
-	}
 
-	[TestClass]
-	public class ConversationTest
-	{
-		[TestMethod]
-		public void TestMethod1()
-		{
+		public void getUsersTest()
+        {
+			Conversation conversation1 = new Conversation("Konfa 1", 1);
+			Conversation conversation2 = new Conversation("Konfa 2", 2);
+			Conversation conversation3 = new Conversation("Konfa 3", 3);
+			User user1 = new User("Pan A");
+			user user2 = new user("Pani B");
 
-		}
+			bool hasUser1 = false;
+			bool hasUser2 = false;
+			bool hasWrongUser = false;
+
+			foreach (var user in conversation1.getUsers())
+            {
+				hasWrongUser = true;
+            }
+			Assert.isFalse(hasWrongUser);
+			hasWrongUser = false;
+			foreach (var user in conversation2.getUsers())
+            {
+				hasWrongUser = true;
+            }
+			Assert.isFalse(hasWrongUser);
+			hasWrongUser = false;
+			foreach (var user in conversation3.getUsers())
+            {
+				hasWrongUser = true;
+            }
+			Assert.isFalse(hasWrongUser);
+			hasWrongUser = false;
+
+			conversation1.matchWithUser(user1);
+			conversation1.matchWithUser(user2);
+			conversation2.matchWithUser(user1);
+
+			foreach (var user in conversation1.getUsers())
+            {
+				if (user == user1)
+					hasUser1 = true;
+				else if (user == user2)
+					hasUser2 = true;
+				else
+					hasWrongUser = true;
+            }
+			Assert.isTrue(hasUser1);
+			Assert.isTrue(hasUser2);
+			Assert.isFalse(hasWrongUser);
+			hasUser1 = false;
+			hasUser2 = false;
+			hasWrongUser = false;
+			foreach (var user in conversation2.getUsers())
+            {
+				if (user == user1)
+					hasUser1 = true;
+				else
+					hasWrongUser = true;
+            }
+			Assert.isTrue(hasUser1);
+			Assert.isTrue(hasUser2);
+			Assert.isFalse(hasWrongUser);
+			hasUser1 = false;
+			hasUser2 = false;
+			hasWrongUser = false;
+			foreach (var user in conversation3.getUsers())
+            {
+				hasWrongUser = true;
+            }
+			Assert.isFalse(hasWrongUser);
+        }
+
+		public void serializeTest() //niekoniecznie tak ten test powinien wyglπdaÊ
+        {
+			ServerChatSystem serverChatSystem = new ServerChatSystem();
+			User user1 = chatSystem.addUser("Jaú Kowalski");
+			User user2 = chatSystem.addUser("Kasia èdüb≥o");
+			Conversation savedConversation = chatSystem.addConversation("Konfa 1", user1, user2);
+			Content msgContent1 = new TextContent("Heeejoooo");
+			Content msgContent2 = new TextContent("Heeej");
+			DateTime datetime = new DateTime(now);
+			Message sentMessage1 = chatSystem.sendMessage(savedConversation.getId(), "Jaú Kowalski", -1, msgContent1, datetime);
+			Message sentMessage2 = chatSystem.sendMessage(savedConversation.getId(), "Kasia èdüb≥o", sentMessage1.getId(), msgContent2, datetime);
+
+			ClientChatSystem clientChatSystem = new ClientChatSystem();
+			User userClient1 = chatSystem.addUser("Jaú Kowalski");
+			User userClient2 = chatSystem.addUser("Kasia èdüb≥o");
+			Conversation savedClientConversation = clientChatSystem.addConversation(savedConversation.serialize());
+
+			bool msg1Present = false;
+			bool msg2Present = false;
+			bool wrongMsgPresent = false;
+			foreach (var message in savedClientConversation.getMessages())
+            {
+				if (message.getId() == sentMessage1.getId())
+                {
+					msg1Present = true;
+					Assert.isTrue(message.getUser().getName() == sentMessage1.getUser().getName());
+					Assert.isNull(message.getParent());
+					Assert.isTrue(message.getContent().getData() == sentMessage1.getContent().getData());
+					Assert.isTrue(message.getTime().Equals(sentMessage1.getTime()));
+                }
+				else if (message.getId() == sentMessage2.getId())
+                {
+					msg2Present = true;
+					Assert.isTrue(message.getUser().getName() == sentMessage2.getUser().getName());
+					Assert.isTrue(message.getParent().getId() == sentMessage1.getId());
+					Assert.isTrue(message.getContent().getData() == sentMessage2.getContent().getData());
+					Assert.isTrue(message.getTime().Equals(sentMessage2.getTime()));
+                }
+				else
+					wrongMsgPresent = true;
+            }
+			Assert.isTrue(msg1Present);
+			Assert.isTrue(msg2Present);
+			Assert.isFalse(wrongMsgPresent);
+        }
+
+		public void getUpdatesTest() //czy tak?
+        {
+			Conversation conversation1 = new Conversation("Konfa 1", 1);
+			User user1 = new User("Mr. X");
+			User user2 = new User("Ms. Y");
+			Content msgContent1 = new TextContent("Heeejoooo");
+			Content msgContent2 = new TextContent("No czeúÊ");
+			Content msgContent3 = new TextContent("Co tam s≥ychaÊ?");
+			DateTime datetime = new DateTime(now);
+			DateTime datetime2 = datetime + 5;
+			DateTime datetime3 = datetime + 19;
+			Message sentMessage1 = conversation1.addMessage(user1, -1, msgContent1, datetime, 1);
+			Message sentMessage2 = conversation1.addMessage(user2, 1, msgContent2, datetime2, 2);
+			Message sentMessage3 = conversation1.addMessage(user1, 2, msgContent3, datetime3, 3);
+
+			bool hasMessage2 = false;
+			bool hasMessage3 = false;
+			bool hasWrongMessage = false;
+			foreach (var message in conversation1.getUpdates(1).getMessages())
+            {
+				if (message.getId() == sentMessage2.getId())
+                {
+					hasMessage2 = true;
+					Assert.isTrue(message.getUser().getName() == sentMessage2.getUser().getName());
+					Assert.isTrue(message.getParent().getId() == 1);
+					Assert.isTrue(message.getContent().getData() == sentMessage2.getContent().getData());
+					Assert.isTrue(message.getTime() == sentMessage2.getTime());
+                }
+				else if (message.getId() == sentMessage2.getId())
+                {
+					hasMessage3 = true;
+					Assert.isTrue(message.getUser().getName() == sentMessage3.getUser().getName());
+					Assert.isTrue(message.getParent().getId() == 2);
+					Assert.isTrue(message.getContent().getData() == sentMessage3.getContent().getData());
+					Assert.isTrue(message.getTime() == sentMessage3.getTime());
+                }
+				else
+					hasWrongMessage = true;
+            }
+			Assert.isTrue(hasMessage2);
+			Assert.isTrue(hasMessage3);
+			Assert.isFalse(hasWrongMessage);
+        }
 	}
 
 	[TestClass]
