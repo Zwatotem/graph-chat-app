819ccacaf64fa2c9785a0b5aa361dc76fddb2aae
zwatotem <zwatotem@gmail.com>
2021-06-07 12:11:06 +0200

    Add comments

--- functionalTests/1.2/runTest.ps1
+++ functionalTests/1.2/runTest.ps1
@@ -1,13 +1,17 @@
+# Executing setup script
 $setup = (gi $PSCommandPath).DirectoryName + '\..\setup.ps1'
 ($projectRoot, $serverPath, $clientPath, $serverExe, $clientExe) = (& $setup)
+# Prepare args for tested programs
 $sArgs = ('127.0.0.1', '50000', '5')
 $cArgs = ('127.0.0.1', '50000')
+# Prepare IO files
 $serverout = (gi $PSCommandPath).DirectoryName + '\server.out'
 $instance1intmpl = (gi $PSCommandPath).DirectoryName + '\instance1.in.template'
 $instance1in = (gi $PSCommandPath).DirectoryName + '\instance1.in'
 $instance1out = (gi $PSCommandPath).DirectoryName + '\instance1.out'
 $instance2in = (gi $PSCommandPath).DirectoryName + '\instance2.in'
 $instance2out = (gi $PSCommandPath).DirectoryName + '\instance2.out'
+# Generate random message text
 $message = [guid]::NewGuid().ToString()
 Set-Content $instance1in (Get-Content $instance1intmpl).Replace("messageText", $message)
 Start-Process -FilePath $serverExe -ArgumentList $sArgs -RedirectStandardOutput $serverout -NoNewWindow
