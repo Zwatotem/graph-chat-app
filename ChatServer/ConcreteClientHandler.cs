550f770a80f3286cb630aa4a21d24261486c7844
zwatotem <zwatotem@gmail.com>
2022-01-12 15:42:53 +0100

    Display user counts

--- ChatServer/ConcreteClientHandler.cs
+++ ChatServer/ConcreteClientHandler.cs
@@ -6,92 +6,115 @@ using ChatModel;
 
 namespace ChatServer
 {
-    /// <summary>
-    /// Concrete implementation of IClientHandler.
-    /// </summary>
-    public class ConcreteClientHandler : IClientHandler
-    {
-        private IIOSocketFacade socketFacade; //socket facade through which communication with client is conducted
-        private string handledUserName;
-        private IServerChatSystem chatSystem; //chatSystem storing and modifying all information about conversations, messages and so forth
-        private List<IClientHandler> allHandlers; //list of all handlers handling connected clients
-        private Thread listenerThread; //thread that is to listen for client requests
-        private bool working;
-        private IRequestHandlerCreator requestHandlerCreator; //contains the factory method necessary for creating instances of IRequestHandler
-        private int requestHeaderLength; //length of request's header - may vary depending on the implementation
+	/// <summary>
+	/// Concrete implementation of IClientHandler.
+	/// </summary>
+	public class ConcreteClientHandler : IClientHandler
+	{
+		private IIOSocketFacade socketFacade; //socket facade through which communication with client is conducted
+		private string handledUserName;
 
-        public string HandledUserName { get => handledUserName; set => handledUserName = value; }
+		private IServerChatSystem
+			chatSystem; //chatSystem storing and modifying all information about conversations, messages and so forth
 
-        public bool Working { get => working; }
+		private List<IClientHandler> allHandlers; //list of all handlers handling connected clients
+		private Thread listenerThread; //thread that is to listen for client requests
+		private bool working;
 
-        public ConcreteClientHandler(IServerChatSystem chatSystem, Socket socket, List<IClientHandler> allHandlers, int headerLength)
-        {
-            Console.WriteLine("DEBUG: handler object created");
-            this.chatSystem = chatSystem;
-            this.socketFacade = new ConcreteIOSocketFacade(socket); //socket is enclosed withing the facade as to simplify its usage
-            this.allHandlers = allHandlers;
-            this.requestHandlerCreator = new ConcreteRequestHandlerCreator();
-            this.requestHeaderLength = headerLength;
-            this.listenerThread = new Thread(this.listen); //listener thread will be executing listen method
-            this.working = false;
-            this.handledUserName = null;
-        }
+		private IRequestHandlerCreator
+			requestHandlerCreator; //contains the factory method necessary for creating instances of IRequestHandler
 
-        public void startWorking()
-        {
-            if (!working) //only start working if it isn't working at the moment
-            {
-                working = true;
-                listenerThread.Start();
-            }
-        }
+		private int requestHeaderLength; //length of request's header - may vary depending on the implementation
 
-        public void sendMessage(byte typeByte, byte[] message)
-        {
-            socketFacade.sendMessage(typeByte, message); //use the socket facade to send a message to client
-        }
+		public string HandledUserName
+		{
+			get => handledUserName;
+			set => handledUserName = value;
+		}
 
-        public void shutdown()
-        {
-            working = false;
-            lock (allHandlers) //prohibit other modifications as the handler stops working 
-            {
-                allHandlers.Remove(this); //remove handler from the server's list of active handlers
-            }
-            socketFacade.shutdown();
-        }
+		public bool Working
+		{
+			get => working;
+		}
 
-        /// <summary>
-        /// Listens for requests from the client and responds accordingly.
-        /// </summary>
-        private void listen()
-        {
-            Console.WriteLine("DEBUG: listener thread started");
-            while (working)
-            {
-                byte typeByte = 0;
-                byte[] messageBytes = null;
-                try
-                {
-                    byte[] headerBytes = socketFacade.receiveMessage(requestHeaderLength);
-                    typeByte = headerBytes[0]; //type byte is the firt one in header
-                    int messageLength = BitConverter.ToInt32(headerBytes, 1); //decode how long the message to receive is
-                    messageBytes = socketFacade.receiveMessage(messageLength);
-                }
-                catch (SocketException ex)
-                {
-                    Console.WriteLine("DEBUG: SocketException thrown: {0}", ex.Message);
-                    typeByte = 0; //if exceptions occured the handler is to shutdown, setting type byte to allow for that
-                }
-                IRequestHandler requestHandler = requestHandlerCreator.createRequestHandler(typeByte); //using factory method to create request
-                //handler. Usage of this design pattern is highly beneficial, as this code is not concerned with what type o handler is necessary.
-                
-                requestHandler.handleRequest(allHandlers, chatSystem, this, messageBytes); //using strategy pattern to handle client request.
-                //This pattern is useful, as we are not concerned with what request have been received. Rather as we received from the factory
-                //method a request handler with the correct instance implementing IHandleStrategy, we can simple call a universal handling method.
-            }
-        }
-    }
+		public ConcreteClientHandler(IServerChatSystem chatSystem, Socket socket, List<IClientHandler> allHandlers,
+			int headerLength)
+		{
+			Console.WriteLine("DEBUG: handler object created");
+			this.chatSystem = chatSystem;
+			this.socketFacade =
+				new ConcreteIOSocketFacade(socket); //socket is enclosed withing the facade as to simplify its usage
+			this.allHandlers = allHandlers;
+			this.requestHandlerCreator = new ConcreteRequestHandlerCreator();
+			this.requestHeaderLength = headerLength;
+			this.listenerThread = new Thread(this.listen); //listener thread will be executing listen method
+			this.working = false;
+			this.handledUserName = null;
+		}
+
+		public void startWorking()
+		{
+			if (!working) //only start working if it isn't working at the moment
+			{
+				working = true;
+				listenerThread.Start();
+			}
+		}
+
+		public void sendMessage(byte typeByte, byte[] message)
+		{
+			socketFacade.sendMessage(typeByte, message); //use the socket facade to send a message to client
+		}
+
+		public void shutdown()
+		{
+			working = false;
+			lock (allHandlers) //prohibit other modifications as the handler stops working 
+			{
+				allHandlers.Remove(this); //remove handler from the server's list of active handlers
+			}
+
+			socketFacade.shutdown();
+		}
+
+		/// <summary>
+		/// Listens for requests from the client and responds accordingly.
+		/// </summary>
+		private void listen()
+		{
+			Console.WriteLine("DEBUG: listener thread started");
+			while (working)
+			{
+				byte typeByte = 0;
+				byte[] messageBytes = null;
+				try
+				{
+					byte[] headerBytes = socketFacade.receiveMessage(requestHeaderLength);
+					typeByte = headerBytes[0]; //type byte is the firt one in header
+					int messageLength =
+						BitConverter.ToInt32(headerBytes, 1); //decode how long the message to receive is
+					messageBytes = socketFacade.receiveMessage(messageLength);
+				}
+				catch (SocketException ex)
+				{
+					Console.WriteLine("DEBUG: SocketException thrown: {0}", ex.Message);
+					if(ex.SocketErrorCode==SocketError.ConnectionAborted)
+						shutdown();
+					typeByte = 0; //if exceptions occured the handler is to shutdown, setting type byte to allow for that
+				}
+
+				IRequestHandler
+					requestHandler =
+						requestHandlerCreator.createRequestHandler(typeByte); //using factory method to create request
+				//handler. Usage of this design pattern is highly beneficial, as this code is not concerned with what type o handler is necessary.
+
+				requestHandler.handleRequest(allHandlers, chatSystem, this,
+					messageBytes); //using strategy pattern to handle client request.
+				//This pattern is useful, as we are not concerned with what request have been received. Rather as we received from the factory
+				//method a request handler with the correct instance implementing IHandleStrategy, we can simple call a universal handling method.
+			}
+		}
+	}
 }
 
 /*
--- GUIChatClient/ViewModel/ConversationViewModel.cs
+++ GUIChatClient/ViewModel/ConversationViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using ChatModel;
 using System;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace GraphChatApp.ViewModel;
@@ -19,23 +21,29 @@ class ConversationViewModel : ChatModel.Util.ViewModel
 {
 	private Conversation conversation;
 	private Action<Conversation> enteringMethod;
+	public int Count => conversation.Users.Count();
 
 	public ConversationViewModel(Conversation conversation, Action<Conversation> action)
 	{
 		this.conversation = conversation;
 		conversation.PropertyChanged += OnPropertyChanged;
+		PropertyChanged += TryUpdateCount;
 		this.enteringMethod = action;
 		EnterCommand = new EnterConversationCommand(EnterConversation);
 	}
 
-	public string Name
+	public string Name => conversation.Name;
+
+	private void TryUpdateCount(object sender, PropertyChangedEventArgs e)
 	{
-		get
+		if(e.PropertyName==nameof(conversation))
 		{
-			return conversation.Name;
+			if (Count != conversation.Users.Count())
+			{
+				OnPropertyChanged(this, new(nameof(Count)));
+			}
 		}
 	}
-
 	public EnterConversationCommand EnterCommand { get; set; }
 	void EnterConversation()
 	{
